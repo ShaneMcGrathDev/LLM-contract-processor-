ceefe4d0f7a174eb37c0a6a7fb77a9e94ea97bf1
Shane <shane.mcgrath.28@gmail.com>
2026-07-25 19:54:25 -0400

    Read railway env variable for PORT

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
 from config import Config
@@ -23,4 +25,5 @@ def create_app():
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
